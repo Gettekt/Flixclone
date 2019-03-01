e1e2f76f34d5f01523414177c64c20fb426d82d2
Christopher Dabalsa <christopherdabalsa@christohersair2.fios-router.home>
2019-02-28 19:26:30 -0500

    Added extra functionality

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Christopher Dabalsa on 2/26/19.
+//  Copyright © 2019 Christopher Dabalsa. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
